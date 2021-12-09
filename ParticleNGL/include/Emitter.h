b5c1f9cf1988d641a4e24d29a96d099521b44587
patipocz <poczkai@gmail.com>
2021-12-09 15:58:02 +0000

    updated to use VAO

---
+++ ParticleNGL/include/Emitter.h
@@ -0,0 +1,30 @@
+#ifndef EMITTER_H_
+#define EMITTER_H_
+#include <vector>
+#include <memory>
+#include <Particle.h>
+#include <ngl/Vec3.h>
+#include <ngl/SimpleVAO.h>
+
+class Emitter
+{
+    public :
+    Emitter()=default;
+    Emitter(size_t _numParticles, const ngl::Vec3 &_emitDir);
+    void update();
+    void render() const ;
+    size_t numParticles() const ;
+    void saveFrame(int _frameNo) const;
+
+    private :
+    void resetParticle(Particle &io_p);
+    std::vector<Particle>m_particles;
+    ngl::Vec3 m_emitDir={0.0f,40.0f,0.0f};
+    float m_spread = 1.9f;
+    std::unique_ptr<ngl::AbstractVAO> m_vao;
+    
+
+
+};
+
+#endif
